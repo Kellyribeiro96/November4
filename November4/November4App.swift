120593cae5ba38ae58ebfef24c349629f0b1ec8f
kelly <kellyribeiro096@icloud.com>
2024-11-04 10:35:24 +0100

    Initial Commit

---
+++ November4/November4App.swift
@@ -0,0 +1,17 @@
+//
+//  November4App.swift
+//  November4
+//
+//  Created by Kelly Karen Ribeiro on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct November4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
